e5a62e09fa062f9c90db10828025b9b37c669682
GODunin <godunin25@yandex,ru>
2022-09-17 14:19:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+     lateinit var bindingClass : ActivityMainBinding
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+}
